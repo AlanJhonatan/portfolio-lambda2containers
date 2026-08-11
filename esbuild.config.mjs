e9e691e015920d4ef6898bebe674394491b4b144
AlanJhonatan <alan.amorim.franca@gmail.com>
2026-08-11 14:43:50 -0300

    feat: add banner param fixing dynamic module import

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -5,7 +5,11 @@ await esbuild.build({
     outfile: 'dist/lambda.mjs',
     platform: 'node',
     target: 'node24',
+    format: 'esm',
     bundle: true,
     minify: true,
     sourcemap: false,
+    banner: {
+        js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
+    },
 })
